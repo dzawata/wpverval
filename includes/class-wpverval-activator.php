da2fadd795578249a4bbc07638e341004e2eec80
Idhar Firmansyah <interisty91@gmail.com>
2021-10-26 15:22:42 +0700

    commit pertama

---
+++ includes/class-wpverval-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/dzawata/
+ * @since      1.0.0
+ *
+ * @package    Wpverval
+ * @subpackage Wpverval/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wpverval
+ * @subpackage Wpverval/includes
+ * @author     Idhar Firmansyah <interisty91@gmail.com>
+ */
+class Wpverval_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wpverval-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/dzawata/
+ * @since      1.0.0
+ *
+ * @package    Wpverval
+ * @subpackage Wpverval/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wpverval
+ * @subpackage Wpverval/includes
+ * @author     Idhar Firmansyah <interisty91@gmail.com>
+ */
+class Wpverval_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
